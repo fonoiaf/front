2967497f79e9be69d9b49f4e842f5ac8c4d6501e
bolicos <apollobolico@gmail.com>
2024-10-05 00:35:37 -0300

    feature: Fix github url.

--- src/app.tsx
+++ src/app.tsx
@@ -19,7 +19,7 @@ export default function App() {
     <Fab
       size="medium"
       aria-label="Github"
-      href="https://github.com/minimal-ui-kit/material-kit-react"
+      href="https://github.com/fonoiaf/front"
       sx={{
         zIndex: 9,
         right: 20,
