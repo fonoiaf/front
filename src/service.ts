add4954cc59892294167718574c34ab8eb84db5f
bolicos <apollobolico@gmail.com>
2025-01-19 23:17:27 -0300

    Fix the URL API was wrong.

--- src/service.ts
+++ src/service.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: process.env.API ?? 'https://fonoiaf-back.onrender.com',
-  baseURL: import.meta.env.BASE_URL ?? 'https://fonoiaf-back.onrender.com',
+  baseURL: import.meta.env.API ?? 'https://fonoiaf-back.onrender.com',
   // timeout: process.env.TIMEOUT ?? 1000,
   timeout: import.meta.env.TIMEOUT ?? 1000,
   headers: { 'X-Custom-Header': 'foobar' },
